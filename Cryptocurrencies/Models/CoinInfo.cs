fe64550adff5e45f50fe95425a81330430d0d238
Andriy Gth <perebeynos19856@gmail.com>
2022-06-23 12:30:11 +0300

    Making models folder and first classes to deserialize

---
+++ Cryptocurrencies/Models/CoinInfo.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Cryptocurrencies.Models
+{
+    class CoinInfo
+    {
+        public string id { get; set; }
+        public int rank { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public decimal supply { get; set; }
+        public decimal? maxSupply { get; set; }
+        public decimal marketCapUsd { get; set; }
+        public decimal volumeUsd24Hr { get; set; }
+        public decimal priceUsd { get; set; }
+        public decimal changePercent24Hr { get; set; }
+        public decimal? vwap24Hr { get; set; }
+
+    }
+}
---
+++ Cryptocurrencies/Models/HttpWorcker.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Cryptocurrencies.Models
+{
+
+    
+    static class HttpWorcker
+    {
+        static HttpClient client = new HttpClient();
+
+        public static async Task<T> GetRequestAsync<T>(string path)
+        {
+            T? data = default(T);
+
+            HttpResponseMessage response = await client.GetAsync(path, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                data = JsonConvert.DeserializeObject<T>(result);
+            }
+
+            return data!;
+        }
+
+        public static CoinInfo[] GetCoinsInfo()
+        {
+            DataArr<CoinInfo> dataArr = HttpWorcker.GetRequestAsync<DataArr<CoinInfo>>(@"https://api.coincap.io/v2/assets").Result;
+            CoinInfo[] coins = dataArr.data;
+            return coins;
+        }
+    }
+}
